b75970dd4de8ebf9468039fb5734c348e4728bd1
Kraviecdev <patrick36212@gmail.com>
2024-08-12 16:10:13 +0200

    create & export validation function

---
+++ src/_utils/validate.ts
@@ -0,0 +1,48 @@
+import { Validatable } from "./types.ts";
+
+const validate = (validatableInput: Validatable) => {
+  let isValid = true;
+
+  //check if required input is not empty
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  //check if input value is greater | equal than minLength (only string)
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  //check if input value is less | equal than maxLength (only string)
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+
+  //check if input value greater | equal than min (only number)
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  //check if input value is less | equal than max (only number)
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+
+  return isValid;
+};
+
+export { validate };
