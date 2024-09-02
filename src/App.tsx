ebf5bd95b5c33cb1012fa216b3ced1a3a0764f2d
Kraviecdev <patrick36212@gmail.com>
2024-09-02 14:01:14 +0200

    implement React state, sections handle form cb, and handle drop function to setting projects finished or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
+import React from "react";
+import { ProjectInput } from "./_utils/interfaces.ts";
 import Form from "./_components/Form";
+import Section from "./_components/Section";
+import List from "./_components/List";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [projects, setProjects] = React.useState<ProjectInput[]>([]);
+
+  const handleFormData = (data: ProjectInput) => {
+    setProjects((prev) => [...prev, data]);
+  };
+
+  const handleItemDrop = (item: ProjectInput) => {
+    //Find project index
+    const projectIndex = projects.findIndex(
+      (project) => project.id === item.id,
+    );
+
+    if (projectIndex !== -1) {
+      //copy projects
+      const updatedProjects = [...projects];
+
+      //select project with found index, copy it and change isActive
+      updatedProjects[projectIndex] = {
+        ...updatedProjects[projectIndex],
+        isActive: !updatedProjects[projectIndex].isActive,
+      };
+
+      //state update
+      setProjects(updatedProjects);
+    }
+  };
+
   return (
     <main>
-      <Form />
+      <Form onFormSubmit={handleFormData} />
+      <Section modifier="active">
+        <List
+          onItemDrop={handleItemDrop}
+          items={projects.filter((project: ProjectInput) => project.isActive)}
+        />
+      </Section>
+      <Section modifier="finished">
+        <List
+          onItemDrop={handleItemDrop}
+          items={projects.filter((project: ProjectInput) => !project.isActive)}
+        />
+      </Section>
     </main>
   );
-}
+};
 
 export default App;
