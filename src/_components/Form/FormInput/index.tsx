512d6ecd8d3880fa5675e74105faa50239cd5c89
Kraviecdev <patrick36212@gmail.com>
2024-09-12 13:29:13 +0200

    change props from to name

--- src/_components/Form/FormInput/index.tsx
+++ src/_components/Form/FormInput/index.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-import { InputI } from "../../../_utils/interfaces.ts";
+import { InputI } from "../../../_utils/types.ts";
 import "./formInput.css";
 
 const FormInput = <T extends HTMLInputElement & HTMLTextAreaElement>({
-  htmlFor,
-  label,
+  name,
   value,
   params,
   textarea,
-  onValueChange,
 }: InputI) => {
   const [inputVal, setInputVal] = React.useState(value);
 
   const handleChange = (e: React.ChangeEvent<T>) => {
     const value = e.target.value;
     setInputVal(value);
-    onValueChange(value);
   };
 
   return (
     <div className="form-control">
-      <label htmlFor={htmlFor} className="form-label">
-        {label}
+      <label htmlFor={name} className="form-label">
+        {name}
       </label>
       {textarea ? (
         <textarea
-          id={htmlFor}
+          id={name}
+          name={name}
           value={inputVal}
           onChange={handleChange}
           {...params}
         />
       ) : (
         <input
-          id={htmlFor}
+          id={name}
+          name={name}
           value={inputVal}
           onChange={handleChange}
           {...params}
