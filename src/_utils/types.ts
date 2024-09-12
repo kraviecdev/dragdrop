5b0ca1ba7c93e80b4cfc0b60af5d70910433d77b
Kraviecdev <patrick36212@gmail.com>
2024-09-12 13:27:52 +0200

    refactor interfaces to types & refactor interfaces to classes

---
+++ src/_utils/types.ts
@@ -0,0 +1,53 @@
+import { nanoid } from "nanoid";
+
+export interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+export interface InputI {
+  name: string;
+  value: string | number | undefined;
+  params: {
+    [key: string]: number | string | boolean;
+  };
+  textarea: boolean;
+}
+
+export interface Data {
+  title?: string;
+  description?: string;
+  people?: number;
+}
+
+export class ProjectInput {
+  id: string;
+  title: string;
+  description: string;
+  people: number;
+  section: string;
+
+  constructor(data: Data) {
+    this.id = nanoid();
+    this.title = data.title || "";
+    this.description = data.description || "";
+    this.people = data.people || 0;
+    this.section = "active";
+  }
+}
+
+export class SectionInput {
+  id: string;
+  title: string;
+  projects: [];
+
+  constructor(data: Data) {
+    this.id = nanoid();
+    this.title = data.title || "";
+    this.projects = [];
+  }
+}
