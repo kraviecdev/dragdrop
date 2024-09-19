cc1abaaff41f4b25f8948f15164577d861f17860
Kraviecdev <patrick36212@gmail.com>
2024-09-19 18:56:11 +0200

    implement button with className & onClickHandler CB

--- src/_components/Section/index.tsx
+++ src/_components/Section/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./section.css";
+import Button from "../Button";
 
 interface SectionProps {
   children: React.ReactNode;
   name: string;
+  onClickHandler: () => void;
 }
 
-const Section = ({ children, name }: SectionProps) => {
+const Section = ({ children, name, onClickHandler }: SectionProps) => {
   return (
     <section className="section">
       <header>
         <h2>{name}</h2>
+        <Button className="delete" name="&#10006;" onClick={onClickHandler} />
       </header>
       {children}
     </section>
